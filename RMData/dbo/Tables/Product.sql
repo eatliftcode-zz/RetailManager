01ca03a38f69d9c2fc77e48a3853b7501f456dd4
Mohit Gupta <mohit.gpta28@Gmail.com>
2021-10-21 07:27:04 -0500

    Created intitial db tables
    Added a trigger to product table to update the LastModified column to latest datetime when the table is updated

---
+++ RMData/dbo/Tables/Product.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductName] NVARCHAR(100) NOT NULL,
+    [RetailPrice] MONEY NOT NULL,
+	[Description] NVARCHAR(MAX) NOT NULL,
+	[CreatedData] DATETIME2 NOT NULL DEFAULT getutcdate(),
+	[LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+
+)
+
+GO
+
+CREATE TRIGGER [dbo].[lastmodified_trigger]
+    ON [dbo].[Product]
+    FOR UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+        UPDATE Product
+        SET LastModified = GETUTCDATE()
+    END
